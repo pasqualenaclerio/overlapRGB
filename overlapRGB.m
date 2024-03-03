b155fc6e5fe02fc7b23a4a4a492cb9a4ce51456f
pasqualenaclerio <ing.nacleriopasquale@gmail.com>
2024-03-03 18:54:12 +0100

    second commit

--- overlapRGB.m
+++ overlapRGB.m
@@ -1,14 +1,25 @@
 clear all; close all;
 
-%%
+%% Test with an original color image
+%  deselect and comment out the RGB overlap part
+%{
+img = imread('images/alpaca.jpg');
+
+red = img(:, :, 1);
+green = img(:, :, 2);
+blue = img(:, :, 3);
+
+Rimg = red;
+Gimg = green;
+Bimg = blue;
+
+Irgb = cat(3,red,green,blue);
+%}
+%% overlap RGB
 
 Rimg = imread('images/ppR.JPG');
 Gimg = imread('images/ppG.JPG');
 Bimg = imread('images/ppB.JPG');
-
-redChannel = Rimg(:, :, 1);
-greenChannel = Gimg(:, :, 2);
-blueChannel = Bimg(:, :, 3);
 
 
 red = Rimg(:, :, 1);
@@ -17,6 +28,39 @@ blue = Bimg(:, :, 3);
     
 
 Irgb = cat(3,red,green,blue);
+
+%% Show RGB images
+
+allBlack = zeros(size(Irgb, 1), size(Irgb, 2), 'uint8');
+
+% Create color versions of the individual color channels.
+just_red = cat(3, red, allBlack, allBlack);
+    %figure('Name', 'image with only the red channel')
+    %imshow(just_red);
+
+just_green = cat(3, allBlack, green, allBlack);
+    %figure('Name', 'image with only the green channel')
+    %imshow(just_green);
+
+just_blue = cat(3, allBlack, allBlack, blue);
+    %figure('Name', 'image with only the blu channel')
+    %imshow(just_blue);
+
+figure('Name', 'the three channels of RGB')
+
+subplot(2,3,1), imshow(Rimg)
+title('s1: red channel info')
+subplot(2,3,2), imshow(Gimg)
+title('s2: green channel info')
+subplot(2,3,3), imshow(Bimg)
+title('s3: blu channel info')
+
+subplot(2,3,4), imshow(just_red)
+title('s4: red channel info color')
+subplot(2,3,5), imshow(just_green)
+title('s5: green channel info color')
+subplot(2,3,6), imshow(just_blue)
+title('s6: blu channel info color')
 
 %% Color Correct with the average
 img_color_avarage = color_correct_avarage(Irgb);
